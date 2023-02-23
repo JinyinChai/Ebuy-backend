524ccd78c346d438289b4b94922ab30df25a73c9
Jingting <xingjingting1217@gmail.com>
2023-02-23 14:46:27 -0800

    create product add/delete/update method

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,39 @@
 const router = require("express").Router();
 const Product = require("../models/Product");
 
+// add new product
+router.post("/newProduct", async (req, res) => {
+    const newProduct = new Product(req.body);
+    try {
+        const savedProduct = await newProduct.save();
+        return res.status(200).json(savedProduct);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
+// delete product
+router.delete("/delete/:id", async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        return res.status(200).json("Product has been deleted.");
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
+
+// update product
+router.put("/update/:id", async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true});
+        return res.status(200).json(updatedProduct);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
+
 // get product by id
 router.get("/find/:id", async (req, res) => {
     try {
@@ -11,7 +44,7 @@ router.get("/find/:id", async (req, res) => {
     }
 })
 
-// get products by title
+// get product by title
 router.get("/findByTitle/:title", async (req, res) => {
     try {
         const products = await Product.find({title: {$regex: ".*" + req.params.title + ".*"}});
